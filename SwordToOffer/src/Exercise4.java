a3462c6bdac1ef3564be28da9b808503b1f161c4
keyuan.dky <keyuan.dky@alibaba-inc.com>
2018-01-21 20:45:39 +0800

    字符串替换

---
+++ SwordToOffer/src/Exercise4.java
@@ -0,0 +1,34 @@
+/*把字符串每个空格替换成%20 */
+public class Exercise4 {
+    public static void main(String[] args) {
+        StringBuffer a =new StringBuffer("we are happy.") ;
+        System.out.println(repalce(a));
+    }
+
+    public static String repalce(StringBuffer original){
+        int n= 0,length=original.length();
+        for (int i = 0; i < length; i++) {
+            if(original.charAt(i)==' '){
+                n++;
+            }
+        }
+        char [] end = new char[2*n+length];
+        for(int i = original.length()-1;i>=0&&n>0;i--){
+            if(original.charAt(i)!=' '){
+                end[i+2*n]=original.charAt(i);
+            }else {
+                end[i+2*n]='0';
+                end[i+2*n-1]='2';
+                end[i+2*n-2]='%';
+                n--;
+            }
+        }
+        String s = new String();
+        for (int i = 0; i < end.length; i++) {
+            s+=end[i];
+        }
+        return s;
+    }
+
+
+}
