182d5f5dab18ed32cab0e089f08928dacd7f97ae
Sampson-2016 <dkynjust@foxmail.com>
2018-01-23 19:56:24 +0800

    两个栈实现队列

---
+++ SwordToOffer/src/Exercise7.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+public class Exercise7
+{
+    /*两个栈实现队列，实现两个函数appendTail和deleteHead，分别完成队列尾部插入结点和在队列头部删除结点功能*/
+    public static void main(String[] args) {
+        MyQueue queue=new MyQueue();
+        queue.appendTail(1);
+        queue.appendTail(2);
+        System.out.println(queue.deleteHead());
+        queue.appendTail(3);
+        System.out.println(queue.deleteHead());
+        System.out.println(queue.deleteHead());
+        System.out.println(queue.deleteHead());
+    }
+
+}
+
+class MyQueue{
+    Stack in;
+    Stack out;
+    public MyQueue(){
+        in = new Stack();
+        out = new Stack();
+    }
+    public void appendTail(Object data){
+        in.push(data);
+    }
+    public Object deleteHead(){
+        if(!out.isEmpty()){
+            return  out.pop();
+        }else if(!in.isEmpty()){
+            while (!in.isEmpty()){
+                out.push(in.pop());
+            }
+            return out.pop();
+        }else {
+            return null; /*队列为空时返回null*/
+        }
+    }
+}
