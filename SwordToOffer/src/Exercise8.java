3eeae25e2c19a4298522bbd9f3c238b9287ab6fa
Sampson-2016 <dkynjust@foxmail.com>
2018-01-25 15:11:44 +0800

    寻找旋转数组最小值

--- SwordToOffer/src/Exercise8.java
+++ SwordToOffer/src/Exercise8.java
@@ -1,28 +1,22 @@
 public class Exercise8 {
     /*旋转数组的最小值*/
     public static void main(String[] args) {
-        int a[]={};
-        System.out.println(getMin(a));
+        int a[]={5,6,7,0,1,2,3,4};
+        System.out.println(getMin(a,0,a.length-1));
     }
 
-    public static int getMin(int[] array) {
-        int length = array.length;
-        if(length==0){
-            try {
-                throw new Exception("数组为空");
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-            return 0;
-        }else if(length==1){
-            return array[0];
+    public static int getMin(int[] array,int start,int end) {
+        if(start==end){
+            return array[start];
         }else {
-            for (int i = length-2; i >=0; i--) {
-                if(array[i]>array[i+1]){
-                    return array[i+1];
-                }
+            int middle = (start+end)/2 ;
+            if(array[start]<array[middle]){
+                return getMin(array,middle+1,end);
+            }else if(array[start]>array[middle]){
+                return getMin(array,start,middle);
+            }else {
+                return getMin(array,start+1,end);
             }
-            return array[0];
         }
     }
 }
